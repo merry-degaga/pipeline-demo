67e771e5c7655fef2f5cd15bbfe8abd9e552d68b
merry-degaga <merry.degaga@revature.net>
2022-01-27 15:43:02 -0500

    updated again app

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -7,7 +7,7 @@ public class App {
     private static Javalin app;
 
     public static void main(String[] args) {
-        app = Javalin.create().start(9090); //This represents the configuration of the framework at runtime.
+        app = Javalin.create(); //This represents the configuration of the framework at runtime.
 
         app.get("/hello", ((ctx) -> {
             String url = ctx.url();
@@ -16,7 +16,7 @@ public class App {
             ctx.html("<h1> Hello, we've got pumpkins</h1>");
             ctx.status(200);
         }));
-
+        app.start(9090);
     }
 
 }
