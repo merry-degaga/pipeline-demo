aec6ab0d2b19cfae4d366b37a55d31ee76e71142
merry-degaga <merry.degaga@revature.net>
2022-01-27 15:13:48 -0500

    pipeline demo

---
+++ src/main/java/com/revature/App.java
@@ -0,0 +1,22 @@
+package com.revature;
+
+import io.javalin.Javalin;
+
+public class App {
+
+    private static Javalin app;
+
+    public static void main(String[] args) {
+        app = Javalin.create().start(9090); //This represents the configuration of the framework at runtime.
+
+        app.get("/hello", ((ctx) -> {
+            String url = ctx.url();
+            System.out.println(url);
+            //the context contains the information in the request / response for the Handler
+            ctx.html("<h1> Hello, we've got pumpkins</h1>");
+            ctx.status(200);
+        }));
+        app.start();
+    }
+
+}
